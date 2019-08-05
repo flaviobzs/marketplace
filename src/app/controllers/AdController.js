62a40dd021e588b295047d593528b8309feccb3d
flaviobzs <bzsflavio@gmail.com>
2019-08-04 22:43:53 -0300

    crud Ad

---
+++ src/app/controllers/AdController.js
@@ -0,0 +1,36 @@
+import Ad from '../models/Ad';
+
+class AdController {
+  async index(req, res) {
+    const ad = await Ad.find();
+
+    return res.json(ad);
+  }
+
+  async show(req, res) {
+    const ad = await Ad.findById(req.params.id);
+
+    return res.json(ad);
+  }
+
+  async store(req, res) {
+    const ad = await Ad.create({ ...req.body, author: req.userId });
+
+    return res.json(ad);
+  }
+
+  async update(req, res) {
+    const ad = await Ad.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    return res.json(ad);
+  }
+
+  async destroy(req, res) {
+    await Ad.findByIdAndDelete(req.params.id);
+
+    return res.send();
+  }
+}
+export default new AdController();
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,21 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import AdController from './app/controllers/AdController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/user', UserController.store);
 
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.get('/ads', AdController.index);
+routes.get('/ads/:id', AdController.show);
+routes.post('/ads', AdController.store);
+routes.put('/ads/:id', AdController.update);
+routes.delete('/ads/:id', AdController.destroy);
 
 export default routes;
